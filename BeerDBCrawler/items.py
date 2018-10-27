f0f01e6634f7c36503b6a10ffd4cd0cd7682b60c
marcelo <marc.amorim6@gmail.com>
2018-10-26 21:16:16 -0400

    Added Beer Object

--- BeerDBCrawler/items.py
+++ BeerDBCrawler/items.py
@@ -7,6 +7,26 @@
 
 import scrapy
 
+class Beer(scrapy.Item):
+    name = scrapy.Field() # field to specify metadata
+    database = scrapy.Field()
+    id = scrapy.Field()
+    brewery = scrapy.Field()
+    rating = scrapy.Field()
+    number_of_ratings = scrapy.Field()
+    ranking = scrapy.Field()
+    number_of_reviews = scrapy.Field()
+    date = scrapy.Field()
+    ibu = scrapy.Field()
+    pDev = scrapy.Field()
+    state = scrapy.Field()
+    country = scrapy.Field()
+    brewery_website = scrapy.Field()
+    style = scrapy.Field()
+    abv = scrapy.Field()
+    availability = scrapy.Field()
+    description = scrapy.Field()
+    pass
 
 class BeerdbcrawlerItem(scrapy.Item):
     # define the fields for your item here like:
