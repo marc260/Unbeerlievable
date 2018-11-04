4590e6ee0e94ca7cf41ea948c61e48b866a5c818
unknown <liny13@rpi.edu>
2018-11-05 04:29:22 +0800

    Added url user input option

---
+++ Unbeerlievable/resources/link.js
@@ -0,0 +1,5 @@
+export default{
+    link: 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAqvSZrQYpF30bs01pLIB8Rb_Y7BWeR6E8',
+    'x-api-key':'ZMoSqqhCP45ar7425Wqyy4B7p9NCMTBr1wSzUnu9'
+
+}
